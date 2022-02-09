fd5f0f009720d7cc51b0f882e1927981f680eab6
paulmski <paulmski@gmail.com>
2022-02-09 17:51:03 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Assignment 1 | COMP1073 Client-Side JavaScript
+
+/* Variables
+-------------------------------------------------- */
+// Create a new speechSynthesis object
+var synth = window.speechSynthesis;
+// Learn more about SpeechSynthesis.speak() at https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/speak
+var textToSpeak = 'This is the text string that you will generate with your script';
+var speakButton = document.querySelector('button');
+
+
+/* Functions
+-------------------------------------------------- */
+function speakNow(string) {
+	// Create a new speech object, attaching the string of text to speak
+	var utterThis = new SpeechSynthesisUtterance(string);
+	// Actually speak the text
+	synth.speak(utterThis);
+}
+
+/* Event Listeners
+-------------------------------------------------- */
+// Onclick handler for the button that speaks the text contained in the above var textToSpeak
+speakButton.onclick = function() {
+	speakNow(textToSpeak);
+}
