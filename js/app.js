1a09824ef8a3165fb1459fdf1ea95375d72402fb
tylerhmiles <thmiles@lakeheadu.ca>
2022-02-10 16:24:30 -0500

    Fixed speech

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ function changeSentencePart(int) {
 	if (sentence[int] > words[int].length) {
 		sentence[int] = 0;
 	}
-	speakNow(words[sentence[int]])
+	speakNow(words[int][sentence[int]]);
 	paragraph.textContent = formSentence();
 }
 
