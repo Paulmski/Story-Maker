fb89bccca33592071d02c81f17db2ede9add4ccd
paulmski <paulmski@gmail.com>
2022-02-10 14:53:53 -0500

    Resolved #10

--- js/app.js
+++ js/app.js
@@ -6,8 +6,14 @@
 var synth = window.speechSynthesis;
 // Learn more about SpeechSynthesis.speak() at https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/speak
 var textToSpeak = 'This is the text string that you will generate with your script';
-var speakButton = document.querySelector('button');
 
+// Arrays used to create the sentence.
+var speakButton = document.querySelector('button');
+var people = ['My mom', 'My dad', 'The mailman', 'My cousin', 'My teacher', 'The Lion', 'The dog'];
+var verbs = ['sat on', 'ate', 'laughed at', 'hugged', 'climbed on', 'saw', 'yelled at'];
+var adjectives = ['a slimy', 'a red', 'a freaky', 'a big', 'a small', 'an ugly', 'a cute'];
+var things = ['goat', 'camel', 'monkey', 'giraffe', 'ape', 'kitten', 'sheep']
+var places = ['on the moon', 'on a mountain', 'in a house', 'in a car', 'on a train', 'on the field', 'in the box'];
 
 /* Functions
 -------------------------------------------------- */
